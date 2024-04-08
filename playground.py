c2f182c828d02c505d16b3eb996a14bd8b386b9c
YongWoo An <dyddn2015@gmail.com>
2024-04-08 23:18:28 +0900

    04.08 공부

--- playground.py
+++ playground.py
@@ -1,61 +1,41 @@
 """
-0 3 5 4 6 9 2 7 8
-7 8 2 1 0 5 6 0 9
-0 6 0 2 7 8 1 3 5
-3 2 1 0 4 6 8 9 7
-8 0 4 9 1 3 5 0 6
-5 9 6 8 2 0 4 1 3
-9 1 7 6 5 2 0 8 0
-6 0 3 7 0 1 9 5 2
-2 5 8 3 9 4 7 6 0
+문제
+효주는 포도주 시식회에 갔다. 그 곳에 갔더니, 테이블 위에 다양한 포도주가 들어있는 포도주 잔이 일렬로 놓여 있었다. 
+효주는 포도주 시식을 하려고 하는데, 여기에는 다음과 같은 두 가지 규칙이 있다.
+
+포도주 잔을 선택하면 그 잔에 들어있는 포도주는 모두 마셔야 하고, 마신 후에는 원래 위치에 다시 놓아야 한다.
+연속으로 놓여 있는 3잔을 모두 마실 수는 없다.
+효주는 될 수 있는 대로 많은 양의 포도주를 맛보기 위해서 어떤 포도주 잔을 선택해야 할지 고민하고 있다. 
+1부터 n까지의 번호가 붙어 있는 n개의 포도주 잔이 순서대로 테이블 위에 놓여 있고, 
+각 포도주 잔에 들어있는 포도주의 양이 주어졌을 때, 효주를 도와 가장 많은 양의 포도주를 마실 수 있도록 하는 프로그램을 작성하시오. 
+
+예를 들어 6개의 포도주 잔이 있고, 각각의 잔에 순서대로 6, 10, 13, 9, 8, 1 만큼의 포도주가 들어 있을 때, 
+첫 번째, 두 번째, 네 번째, 다섯 번째 포도주 잔을 선택하면 총 포도주 양이 33으로 최대로 마실 수 있다.
+
+입력
+첫째 줄에 포도주 잔의 개수 n이 주어진다. (1 ≤ n ≤ 10,000) 
+둘째 줄부터 n+1번째 줄까지 포도주 잔에 들어있는 포도주의 양이 순서대로 주어진다. 
+포도주의 양은 1,000 이하의 음이 아닌 정수이다.
+
+출력
+첫째 줄에 최대로 마실 수 있는 포도주의 양을 출력한다.
+
+예제 입력 1 
+6
+6
+10
+13
+9
+8
+1
+예제 출력 1 
+33
 """
-rows = [set() for _ in range(9)]
-columns = [set() for _ in range(9)]
-blocks = [set() for _ in range(9)]
-arr = [[0]*9 for _ in range(9)]
-coords = []
-for i in range(9):
-    row = [*map(int, input().split())]
-    for j in range(9):
-        if row[j]==0: coords.append((i, j))
-        arr[i][j] = row[j]
-        rows[i].add(row[j])
-        columns[j].add(row[j])
-        blocks[(i//3)*3+j//3].add(row[j])
-
-def f(i: int):
-    global rows, columns, blocks, arr, coords
-    x, y = coords[i]
-    for j in range(1, 10):
-        if j not in rows[x] and\
-           j not in columns[y] and\
-           j not in blocks[(x//3)*3+y//3]:
-            arr[x][y] = j
-            if i==len(coords)-1:
-                print("\n".join([" ".join(map(str, i)) for i in arr]))
-                exit(0)
-            else:
-                rows[x].add(j)
-                columns[y].add(j)
-                blocks[(x//3)*3+y//3].add(j)
-                f(i+1)
-                rows[x].remove(j)
-                columns[y].remove(j)
-                blocks[(x//3)*3+y//3].remove(j)
-f(0)
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
+import sys
+input()
+p = [0]*3
+for i in sys.stdin:
+    x = int(i)
+    p = [p[1]+x, p[2], p[0]+x]
+print(max(p))
+    
