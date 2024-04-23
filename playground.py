4f13883bdc5f0959abde8277c5d1986a0e25e681
YongWoo An <dyddn2015@gmail.com>
2024-04-23 22:07:48 +0900

    04.23 공부

--- playground.py
+++ playground.py
@@ -1,93 +1,143 @@
 """
-문제
-널리 잘 알려진 자료구조 중 최소 힙이 있다. 최소 힙을 이용하여 다음과 같은 연산을 지원하는 프로그램을 작성하시오.
+이중 우선순위 큐(dual priority queue)는 전형적인 우선순위 큐처럼 데이터를 삽입, 삭제할 수 있는 자료 구조이다. 
+전형적인 큐와의 차이점은 데이터를 삭제할 때 연산(operation) 명령에 따라 
+우선순위가 가장 높은 데이터 또는 가장 낮은 데이터 중 하나를 삭제하는 점이다. 
+이중 우선순위 큐를 위해선 두 가지 연산이 사용되는데, 
+하나는 데이터를 삽입하는 연산이고 다른 하나는 데이터를 삭제하는 연산이다. 
+데이터를 삭제하는 연산은 또 두 가지로 구분되는데 
+하나는 우선순위가 가장 높은 것을 삭제하기 위한 것이고 
+다른 하나는 우선순위가 가장 낮은 것을 삭제하기 위한 것이다.
 
-배열에 자연수 x를 넣는다.
-배열에서 가장 작은 값을 출력하고, 그 값을 배열에서 제거한다.
-프로그램은 처음에 비어있는 배열에서 시작하게 된다.
+정수만 저장하는 이중 우선순위 큐 Q가 있다고 가정하자. Q에 저장된 각 정수의 값 자체를 우선순위라고 간주하자.
+
+Q에 적용될 일련의 연산이 주어질 때 이를 처리한 후 
+최종적으로 Q에 저장된 데이터 중 최댓값과 최솟값을 출력하는 프로그램을 작성하라.
 
 입력
-첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 
-다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 
-만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, 
-x가 0이라면 배열에서 가장 작은 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 
-x는 231보다 작은 자연수 또는 0이고, 음의 정수는 입력으로 주어지지 않는다.
+입력 데이터는 표준입력을 사용한다. 입력은 T개의 테스트 데이터로 구성된다. 
+입력의 첫 번째 줄에는 입력 데이터의 수를 나타내는 정수 T가 주어진다. 
+각 테스트 데이터의 첫째 줄에는 Q에 적용할 연산의 개수를 나타내는 정수 k (k ≤ 1,000,000)가 주어진다. 
+이어지는 k 줄 각각엔 연산을 나타내는 문자(‘D’ 또는 ‘I’)와 정수 n이 주어진다. 
+‘I n’은 정수 n을 Q에 삽입하는 연산을 의미한다. 동일한 정수가 삽입될 수 있음을 참고하기 바란다. 
+‘D 1’는 Q에서 최댓값을 삭제하는 연산을 의미하며, ‘D -1’는 Q 에서 최솟값을 삭제하는 연산을 의미한다. 
+최댓값(최솟값)을 삭제하는 연산에서 최댓값(최솟값)이 둘 이상인 경우, 하나만 삭제됨을 유념하기 바란다.
+
+만약 Q가 비어있는데 적용할 연산이 ‘D’라면 이 연산은 무시해도 좋다. 
+Q에 저장될 모든 정수는 -2^31 이상 2^31 미만인 정수이다.
 
 출력
-입력에서 0이 주어진 횟수만큼 답을 출력한다. 
-만약 배열이 비어 있는 경우인데 가장 작은 값을 출력하라고 한 경우에는 0을 출력하면 된다.
+출력은 표준출력을 사용한다. 각 테스트 데이터에 대해, 모든 연산을 처리한 후 Q에 남아 있는 값 중 최댓값과 최솟값을 출력하라. 
+두 값은 한 줄에 출력하되 하나의 공백으로 구분하라. 만약 Q가 비어있다면 ‘EMPTY’를 출력하라.
 
 예제 입력 1 
+2
+7
+I 16
+I -5643
+D -1
+D 1
+D 1
+I 123
+D -1
 9
-0
-12345678
-1
-2
-0
-0
-0
-0
-32
+I -45
+I 653
+D 1
+I -642
+I 45
+I 97
+D 1
+D -1
+I 333
 예제 출력 1 
-0
-1
-2
-12345678
-0
-
-
-1
-10
-2
-30
-40
-300
-400
-25
-0
-0
+EMPTY
+333 -45
 """
 from sys import stdin
-from collections import deque
-stdin.readline()
-heap = deque()
-for i in stdin:
-    option = int(i)
-    if option:
-        heap.append(option)
-        child = len(heap)-1
-        parent = (child-1)//2
-        while child and heap[child] < heap[parent]:
-            heap[parent], heap[child] = heap[child], heap[parent]
-            child = parent
-            parent = (child-1)//2
-    else:
-        if len(heap)>1:
-            print(heap.popleft())
-            heap.appendleft(heap.pop())
-            parent = 0
-            last_idx = len(heap)-1
-            while parent < last_idx:
-                l_child = parent*2+1
-                r_child = parent*2+2
-                if r_child <= last_idx:
-                    if heap[l_child] <= heap[r_child] and heap[l_child] < heap[parent]:
-                        heap[parent], heap[l_child] = heap[l_child], heap[parent]
-                        parent = l_child
-                    elif heap[r_child] < heap[l_child] and heap[r_child] < heap[parent]:
-                        heap[parent], heap[r_child] = heap[r_child], heap[parent]
-                        parent = r_child
+
+class Node:
+    def __init__(self, key, left = None, right = None):
+        self.key = key
+        self.left = left
+        self.right = right
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def is_not_empty(self):
+        return self.root is not None
+
+    def add(self, key):
+        if self.root:
+            node = self.root
+            while 1:
+                if node.key > key:
+                    if node.left:
+                        node = node.left
                     else:
+                        node.left = Node(key)
                         break
-                elif l_child <= last_idx and heap[l_child] < heap[parent]:
-                    heap[parent], heap[l_child] = heap[l_child], heap[parent]
-                    parent = l_child
                 else:
-                    break
-        elif len(heap)==1:
-            print(heap.pop())
+                    if node.right:
+                        node = node.right
+                    else:
+                        node.right = Node(key)
+                        break
         else:
-            print(0)
+            self.root = Node(key)
+
+    def min_key(self) -> str:
+        min_node = self.root
+        while min_node.left: 
+            min_node = min_node.left
+        return str(min_node.key)
+
+    def max_key(self) -> str:
+        max_node = self.root
+        while max_node.right: 
+            max_node = max_node.right
+        return str(max_node.key)
+
+    def pop_min(self) -> None:
+        parent = self.root
+        min_node = parent.left
+        while min_node and min_node.left: 
+            parent = min_node
+            min_node = min_node.left
+        if min_node is None:
+            self.root = parent.right
+        elif min_node.right:
+            parent.left = min_node.right
+        else:
+            parent.left = None
+
+    def pop_max(self) -> None:
+        parent = self.root
+        max_node = parent.right
+        while max_node and max_node.right: 
+            parent = max_node
+            max_node = max_node.right
+        if max_node is None:
+            self.root = parent.left
+        elif max_node.left:
+            parent.right = max_node.left
+        else:
+            parent.right = None
+
+for _ in range(int(stdin.readline())):
+    tree = BinarySearchTree()
+    for _ in range(int(stdin.readline())):
+        o, x = stdin.readline().strip().split()
+        if o=="I":
+            tree.add(int(x))
+        else:
+            if tree.is_not_empty():
+                if x=="1":
+                    tree.pop_max()
+                else:
+                    tree.pop_min()
+    print(" ".join((tree.max_key(), tree.min_key())) if tree.is_not_empty() else "EMPTY")
 
 
 
