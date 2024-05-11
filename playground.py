7f37d2e7df4f270f355bf95401f6e6bbc08f0c85
YongWoo An <dyddn2015@gmail.com>
2024-05-11 20:54:34 +0900

    05.11 공부

--- playground.py
+++ playground.py
@@ -1,82 +1,56 @@
 """
 문제
-때는 2020년, 백준이는 월드나라의 한 국민이다. 월드나라에는 N개의 지점이 있고 N개의 지점 사이에는 M개의 도로와 W개의 웜홀이 있다. 
-(단 도로는 방향이 없으며 웜홀은 방향이 있다.) 웜홀은 시작 위치에서 도착 위치로 가는 하나의 경로인데, 
-특이하게도 도착을 하게 되면 시작을 하였을 때보다 시간이 뒤로 가게 된다. 웜홀 내에서는 시계가 거꾸로 간다고 생각하여도 좋다.
+N줄에 0 이상 9 이하의 숫자가 세 개씩 적혀 있다. 
+내려가기 게임을 하고 있는데, 이 게임은 첫 줄에서 시작해서 마지막 줄에서 끝나게 되는 놀이이다.
 
-시간 여행을 매우 좋아하는 백준이는 한 가지 궁금증에 빠졌다. 
-한 지점에서 출발을 하여서 시간여행을 하기 시작하여 다시 출발을 하였던 위치로 돌아왔을 때, 
-출발을 하였을 때보다 시간이 되돌아가 있는 경우가 있는지 없는지 궁금해졌다. 
-여러분은 백준이를 도와 이런 일이 가능한지 불가능한지 구하는 프로그램을 작성하여라.
+먼저 처음에 적혀 있는 세 개의 숫자 중에서 하나를 골라서 시작하게 된다. 
+그리고 다음 줄로 내려가는데, 다음 줄로 내려갈 때에는 다음과 같은 제약 조건이 있다. 
+바로 아래의 수로 넘어가거나, 아니면 바로 아래의 수와 붙어 있는 수로만 이동할 수 있다는 것이다. 
+이 제약 조건을 그림으로 나타내어 보면 다음과 같다.
+
+
+
+별표는 현재 위치이고, 그 아랫 줄의 파란 동그라미는 원룡이가 다음 줄로 내려갈 수 있는 위치이며, 
+빨간 가위표는 원룡이가 내려갈 수 없는 위치가 된다. 숫자표가 주어져 있을 때, 
+얻을 수 있는 최대 점수, 최소 점수를 구하는 프로그램을 작성하시오. 점수는 원룡이가 위치한 곳의 수의 합이다.
 
 입력
-첫 번째 줄에는 테스트케이스의 개수 TC(1 ≤ TC ≤ 5)가 주어진다. 
-그리고 두 번째 줄부터 TC개의 테스트케이스가 차례로 주어지는데 
-각 테스트케이스의 첫 번째 줄에는 지점의 수 N(1 ≤ N ≤ 500), 도로의 개수 M(1 ≤ M ≤ 2500), 웜홀의 개수 W(1 ≤ W ≤ 200)이 주어진다. 
-그리고 두 번째 줄부터 M+1번째 줄에 도로의 정보가 주어지는데 각 도로의 정보는 S, E, T 세 정수로 주어진다. 
-S와 E는 연결된 지점의 번호, T는 이 도로를 통해 이동하는데 걸리는 시간을 의미한다. 
-그리고 M+2번째 줄부터 M+W+1번째 줄까지 웜홀의 정보가 S, E, T 세 정수로 주어지는데 S는 시작 지점, E는 도착 지점, T는 줄어드는 시간을 의미한다. T는 10,000보다 작거나 같은 자연수 또는 0이다.
-
-두 지점을 연결하는 도로가 한 개보다 많을 수도 있다. 지점의 번호는 1부터 N까지 자연수로 중복 없이 매겨져 있다.
+첫째 줄에 N(1 ≤ N ≤ 100,000)이 주어진다. 
+다음 N개의 줄에는 숫자가 세 개씩 주어진다. 
+숫자는 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 중의 하나가 된다.
 
 출력
-TC개의 줄에 걸쳐서 만약에 시간이 줄어들면서 출발 위치로 돌아오는 것이 가능하면 YES, 불가능하면 NO를 출력한다.
+첫째 줄에 얻을 수 있는 최대 점수와 최소 점수를 띄어서 출력한다.
 
 예제 입력 1 
-2
-3 3 1
-1 2 2
-1 3 4
-2 3 1
-3 1 3
-3 2 1
+3
 1 2 3
-2 3 4
-3 1 8
+4 5 6
+4 9 0
 예제 출력 1 
-NO
-YES
+18 6
+예제 입력 2 
+3
+0 0 0
+0 0 0
+0 0 0
+예제 출력 2 
+0 0
 """
+from collections import deque
 from sys import stdin
-INF = 10**8
 
-for _ in range(int(stdin.readline())):
-    N, R, W = map(int, stdin.readline().split())
-    lst = [{} for _ in range(N+1)]
-    for _ in range(R):
-        fr, to, time = map(int, stdin.readline().split())
-        if to in lst[fr]:
-            if time < lst[fr][to]:
-                lst[fr][to] = time
-        else:
-            lst[fr][to] = time
-        if fr in lst[to]:
-            if time < lst[to][fr]:
-                lst[to][fr] = time
-        else:
-            lst[to][fr] = time
-    for _ in range(W):
-        fr, to, time = map(int, stdin.readline().split())
-        lst[fr][to] = -time
-    
-    check = [INF]*(N+1)
-    check[1] = 0
-    for i in range(N):
-        for fr in range(1, N+1):
-            for to, time in lst[fr].items():
-                if check[to] > check[fr] + time:
-                    check[to] = check[fr] + time
-                    if i == N-1:
-                        print("YES")
-                        break
-            else:
-                continue
-            break
-        else:
-            continue
-        break
-    else:
-        print("NO")
-    
-
-
+N = int(stdin.readline())
+min_values = [0, 0, 0]
+max_values = [0, 0, 0]
+for _ in range(N):
+    v1, v2, v3 = map(int, stdin.readline().split())
+    m1, m2, m3 = min_values
+    M1, M2, M3 = max_values
+    min_values[0] = min(m1, m2) + v1
+    min_values[1] = min(m1, m2, m3) + v2
+    min_values[2] = min(m2, m3) + v3
+    max_values[0] = max(M1, M2) + v1
+    max_values[1] = max(M1, M2, M3) + v2
+    max_values[2] = max(M2, M3) + v3
+print(max(max_values), min(min_values))
